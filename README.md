8b7d314199765904f1b9b8e5618027bf6e9039b0
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-11-17 15:39:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+#Sentiment Analysis with Email Notification
+This Python script utilizes the Streamlit library to create a simple Sentiment Analysis application. The sentiment analysis is performed on both user-input text and CSV files containing text data. The results are displayed in a Streamlit app, and users have the option to download the sentiment analysis results as a CSV file. Additionally, users can provide an email address to receive the overall sentiment analysis results via email.
+
+#How to Use
+Clone the Repository: sentiment analysis
+
+bash
+Copy code
+git clone https://github.com/ishuoncode/sentiment-analysis.git
+cd your-repo
+Install Dependencies:
+Ensure that you have Python installed. Install the required packages using:
+
+bash
+Copy code
+pip install -r requirements.txt
+Run the Application:
+Execute the script with the following command:
+
+bash
+Copy code
+streamlit run main.py
+Use the Application:
+
+The Streamlit app will open in your default web browser.
+Enter the receiver's email address in the provided text input.
+Analyze text sentiment by entering text in the "Analyze Text" section.
+Upload a CSV file containing a column named 'tweets' to perform sentiment analysis on the CSV data.
+Receive Email Notification:
+
+If you provided a receiver's email address, the overall sentiment analysis results will be sent to that email.
+Notes
+The application uses the TextBlob library for sentiment analysis.
+The overall sentiment label and CSV file with sentiment analysis results are available for download.
+Ensure that you have allowed "Less secure app access" or used an "App Password" for the sender's email account.
+Update the sender's email and password in the script with your own email credentials.
+Feel free to customize and enhance the application based on your needs!
+
+#Reference
+1. For ML learning: https://www.youtube.com/watch?v=-YCDhHVNNLA&t=4s
+2. For generating your app password and email: https://www.youtube.com/watch?v=6ANKk9NQ3GI
---
+++ main.py
@@ -0,0 +1,113 @@
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from textblob import TextBlob
+import pandas as pd
+import streamlit as st
+
+
+# Streamlit app
+st.header('Sentiment Analysis')
+
+# Input receiver's email
+receiver_email = st.text_input('Enter receiver\'s email:')
+
+# Function to send email with attachment
+sender_email = "REPLACE IT WITH YOUR EMAIL" # Replace it!! 
+password = "REPLACE IT WITH YOUR PASSWORD"  # Replace it!!
+def send_email(subject, body, attachment_path):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(sender_email, password)
+
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+
+        message.attach(MIMEText(body, "plain"))
+
+        # Attach the sentiment file
+        with open(attachment_path, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+            encoders.encode_base64(part)
+            part.add_header("Content-Disposition", f"attachment; filename= {attachment_path}")
+            message.attach(part)
+
+        # Send email
+        smtp.sendmail(sender_email, receiver_email, message.as_string())
+
+# Analyze Text
+with st.expander('Analyze Text'):
+    text = st.text_input('Text here: ')
+    if text:
+        blob = TextBlob(text)
+        st.write('Polarity: ', round(blob.sentiment.polarity, 2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity, 2))
+
+# Analyze CSV
+with st.expander('Analyze CSV'):
+    upl = st.file_uploader('Upload file')
+
+    def score(x):
+        blob1 = TextBlob(x)
+        return blob1.sentiment.polarity
+
+    def analyze(x):
+        if x >= 0.5:
+            return 'Positive'
+        elif x <= -0.5:
+            return 'Negative'
+        else:
+            return 'Neutral'
+
+    if upl:
+        df = pd.read_csv(upl)
+
+        df['score'] = df['tweets'].apply(score)
+        df['analysis'] = df['score'].apply(analyze)
+
+        # Show top 5 comments with highest score
+        st.subheader('Top 5 Comments with Highest Score:')
+        top_comments = df.nlargest(5, 'score')
+        st.table(top_comments[['tweets', 'score', 'analysis']])
+
+        # Show bottom 5 comments with least score
+        st.subheader('Bottom 5 Comments with Least Score:')
+        bottom_comments = df.nsmallest(5, 'score')
+        st.table(bottom_comments[['tweets', 'score', 'analysis']])
+
+        # Calculate and display overall sentiment
+        overall_sentiment = df['score'].mean()
+        overall_sentiment_label = analyze(overall_sentiment)
+        st.subheader('Overall Sentiment:')
+        st.write(f'The overall sentiment for the entire dataset is: {round(overall_sentiment, 2)}')
+        st.write(f'Sentiment Label: {overall_sentiment_label}')
+
+        # Save the DataFrame to a CSV file
+        sentiment_file_path = 'sentiment.csv'
+        df.to_csv(sentiment_file_path, index=False)
+
+        # Send email with overall sentiment label and sentiment file attachment
+        if receiver_email:
+            send_email("Sentiment Analysis Result", f"The overall sentiment is: {overall_sentiment_label}", sentiment_file_path)
+
+        @st.cache
+        def convert_df(df):
+            return df.to_csv().encode('utf-8')
+
+        csv = convert_df(df)
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='sentiment.csv',
+            mime='text/csv',
+        )
